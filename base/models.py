cdb011631e6118260b65657fa6571aa2322df5f9
DeepakSilaych <deepaksilaych@gmail.com>
2024-09-04 20:53:29 +0530

    goooo

---
+++ base/models.py
@@ -0,0 +1,105 @@
+from django.db import models
+
+#----------- Clubs, Tech Team and Other Bodies
+class Body(models.Model):
+  name = models.CharField(max_length=100)
+  type = models.IntegerField(choices=[(0, 'Clubs'), (1, 'Tech Team'), (2, 'Other Bodies')])
+  short_description = models.CharField(max_length=100)
+  description = models.TextField()
+  contact_email = models.EmailField()
+  instagram = models.URLField(blank=True, null=True)
+  facebook = models.URLField(blank=True, null=True)
+  github = models.URLField(blank=True, null=True)
+  linkedin = models.URLField(blank=True, null=True)
+  website = models.URLField(blank=True, null=True)
+  logo = models.ImageField(upload_to='logos/', blank=True)
+
+  def __str__(self):
+    return self.name
+
+class Member(models.Model):
+  body = models.ForeignKey('Body', on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  position = models.CharField(max_length=100)
+  priority = models.IntegerField(default=0, min_value=0, max_value=10)
+  image = models.ImageField(upload_to='members/', blank=True)
+
+  def __str__(self):
+    return self.name + ' - ' + self.position
+  
+#----------- Achievements 
+class Achievement (models.Model):
+  body = models.ForeignKey('Body', on_delete=models.CASCADE)
+  date = models.DateField()
+  title = models.CharField(max_length=100)
+  description = models.TextField()
+
+  def __str__(self):
+    return self.title
+
+
+#----------- Portals and TechStacks
+class Portal(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField()
+  link = models.URLField()
+  banner = models.ImageField(upload_to='banners/', blank=True)
+  techstacks = models.ManyToManyField('TechStack')
+
+  def __str__(self):
+    return self.name
+  
+class TechStack(models.Model):
+  name = models.CharField(max_length=100)
+  logo = models.ImageField(upload_to='techstack/', blank=True)
+
+  def __str__(self):
+    return self.name
+  
+# -------- cabinet and contacts
+class Cabinet(models.Model):
+  name = models.CharField(max_length=100)
+  position = models.CharField(max_length=100)
+  priority = models.IntegerField(default=0, min_value=0, max_value=10)
+  image = models.ImageField(upload_to='cabinet/', blank=True)
+
+  def __str__(self):
+    return self.name + ' - ' + self.position
+
+
+#-----------  WorkReports
+
+class WorkReport(models.Model):
+  title = models.CharField(max_length=100)
+  image = models.ImageField(upload_to='workreports/')
+  url = models.URLField()
+
+  def __str__(self):
+    return self.title
+  
+
+#-----------  Hall of Fame
+
+class InterIIT(models.Model):
+  logo = models.ImageField(upload_to='interiit/')
+  title = models.CharField(max_length=100)
+  description = models.TextField()
+  img = models.ImageField(upload_to='interiit/')
+  gold = models.IntegerField(default=0)
+  silver = models.IntegerField(default=0)
+  bronze = models.IntegerField(default=0)  
+
+  def __str__(self):
+    return self.year + ' - ' + self.location + ' - ' + self.position
+  
+
+class ProblemStatements(models.Model):
+  interiit = models.ForeignKey('InterIIT', on_delete=models.CASCADE)
+  title = models.CharField(max_length=100)
+  description = models.TextField()
+  position = models.IntegerField(default=0)
+  img = models.ImageField(upload_to='problemstatements/')
+  link = models.URLField()
+
+  def __str__(self):
+    return self.title
---
+++ base/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('clubs', views.club_list, name='club_list'),
+    path('tectteam/', views.techteam_list, name='techteam_list'),
+    path('otherbodies/', views.otherbodies_list, name='otherbodies_list'),
+    path('bodies/<str:name>/', views.body_detail, name='body_detail'),
+    path('portals/', views.portal_list, name='portal_list'),
+    path('achievement/', views.achievement_list, name='achievement_list'),
+    path('halloffame/', views.halloffame_list, name='halloffame_list'),
+    path('contact/', views.contact, name='contact'),
+
+]
