e08c4444f4d8ef78c7bb3f49e3d1bd5a04d83c11
DeepakSilaych <deepaksilaych@gmail.com>
2024-10-06 22:55:58 +0530

    a

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
